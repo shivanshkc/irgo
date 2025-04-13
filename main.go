8bfca740c4dbe59d1342811b22934990a3ed4663
Shivansh Kuchchal <shivanshbox@gmail.com>
2025-04-13 18:20:05 +0530

    refactor(core): refactoring, add phi-1m.txt

--- main.go
+++ main.go
@@ -10,17 +10,16 @@ import (
 
 var screenSize = [2]float64{1920, 1080}
 
-//go:embed nums/phi-100k.txt
-var phi string
-
-//go:embed nums/pi-100k.txt
-var pi string
+//go:embed nums/phi-1M.txt
+var num string
 
 func main() {
-	ebiten.SetWindowSize(int(screenSize[0]), int(screenSize[1]))
-	ebiten.SetWindowTitle("Irrational Number Visualization - Golden Ratio")
+	var name = "Phi-1M"
 
-	if err := ebiten.RunGame(pkg.NewIrrationalPlotter("Phi", phi, screenSize)); err != nil {
+	ebiten.SetWindowSize(int(screenSize[0]), int(screenSize[1]))
+	ebiten.SetWindowTitle("Irrational Number Visualization - " + name)
+
+	if err := ebiten.RunGame(pkg.NewIrrationalPlotter(name, num, screenSize)); err != nil {
 		panic("error in ebiten.RunGame: " + err.Error())
 	}
 }
